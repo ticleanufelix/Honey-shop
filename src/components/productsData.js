734940798e71aba59997aeaecb0eb7675a583f0c
ticleanufelix <137090875+ticleanufelix@users.noreply.github.com>
2023-10-19 23:10:56 +0300

    test 12

--- src/components/productsData.js
+++ src/components/productsData.js
@@ -5,7 +5,7 @@ const productsData = [
     description:
       "Mierea de anghinare are aromă și gust caracteristic, fiind recoltată de albine din florile spontane, înflorite în condiții favorabile în a doua parte a verii pe islazurile sau terasele din lunca Dunării.",
     price: 20,
-    imageURL: "../../assets/articol1.png",
+    imageURL: require("./assets/miere-anghinare.png").default,
     category: "miere",
   },
   {
