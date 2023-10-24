87091ace8de09f12b99e966dee919ef4659923d7
ticleanufelix <137090875+ticleanufelix@users.noreply.github.com>
2023-10-24 13:46:24 +0300

    check out and finish madified

--- src/pages/Finish/Finish.jsx
+++ src/pages/Finish/Finish.jsx
@@ -4,23 +4,25 @@ import Navbar from "../../components/navbar/Navbar";
 import "./finishStyles.css";
 function Finish() {
   return (
-    <div className="finsh-all">
+    <div>
       <Navbar />
-      <div className="bug">
-        <div className="bee1">
-          <div className="eye"></div>
+      <div className="finish-all">
+        <div className="bug">
+          <div className="bee1">
+            <div className="eye"></div>
+          </div>
+          <div className="bee2">
+            <div className="wing"></div>
+            <div className="wing2"></div>
+          </div>
+          <div className="black"></div>
         </div>
-        <div className="bee2">
-          <div className="wing"></div>
-          <div className="wing2"></div>
+        <div className="content-finish">
+          <p>Felicitari, comanda ta a fost plasata!!</p>
+          <Link to={"/produse"}>
+            <button className="btn">Intoarce-te la cumparaturi</button>
+          </Link>
         </div>
-        <div className="black"></div>
-      </div>
-      <div className="content-finish">
-        <p>Felicitari, comanda ta a fost plasata!!</p>
-        <Link to={"/produse"}>
-          <button className="btn">Intoarce-te la cumparaturi</button>
-        </Link>
       </div>
       <Footer />
     </div>
