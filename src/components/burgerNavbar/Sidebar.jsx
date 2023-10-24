cf521d5a3235ff48819baf627630d1d7b6074f2f
ticleanufelix <137090875+ticleanufelix@users.noreply.github.com>
2023-10-24 13:56:51 +0300

    scroll to top sideBar

--- src/components/burgerNavbar/Sidebar.jsx
+++ src/components/burgerNavbar/Sidebar.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import Cart from "../cart/Cart";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useCart } from "../../context/CartContext";
 const Sidebar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { scrollToTop } = useCart();
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
@@ -15,19 +17,19 @@ const Sidebar = () => {
       <div className="menu">
         <div>
           <Menu>
-            <Link to="/">
+            <Link to="/" onClick={scrollToTop}>
               <button className="menu-item">Home</button>
             </Link>
-            <Link to={"/produse"}>
+            <Link to={"/produse"} onClick={scrollToTop}>
               <button className="menu-item">Produse</button>
             </Link>
-            <Link to={"/articole"}>
+            <Link to={"/articole"} onClick={scrollToTop}>
               <button className="menu-item">Articole</button>
             </Link>
-            <Link to={"/despre-noi"}>
+            <Link to={"/despre-noi"} onClick={scrollToTop}>
               <button className="menu-item">Despre noi</button>
             </Link>
-            <Link to={"/contact"}>
+            <Link to={"/contact"} onClick={scrollToTop}>
               <button className="menu-item">Contact</button>
             </Link>
           </Menu>
